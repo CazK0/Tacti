e282730c03a1776d64b60a3502be6dc288440fc7
Carrick Kennedy <dkkmschxcw@privaterelay.appleid.com>
2026-02-28 22:45:23 +0000

    Update prediction logic to include draw probability

--- Tacti/app.py
+++ Tacti/app.py
@@ -7,41 +7,38 @@ app = Flask(__name__)
 model = joblib.load('cortex_model.pkl')
 df = pd.read_csv('match_features.csv')
 
-teams = sorted(set(df['HomeTeam'].unique()) | set(df['AwayTeam'].unique()))
-
+recent_matches = df.tail(380)
+teams = sorted(set(recent_matches['HomeTeam'].unique()) | set(recent_matches['AwayTeam'].unique()))
 
 def get_latest_form(team, data):
     past_games = data[(data['HomeTeam'] == team) | (data['AwayTeam'] == team)].tail(3)
-
+    
     if len(past_games) == 0:
         return 0, 0, 0
-
+    
     pts = 0
     gs = 0
     gc = 0
-
+    
     for _, row in past_games.iterrows():
         if row['HomeTeam'] == team:
-            if row['FTR'] == 'H':
-                pts += 3
-            elif row['FTR'] == 'D':
-                pts += 1
+            if row['FTR'] == 'H': pts += 3
+            elif row['FTR'] == 'D': pts += 1
             gs += row['FTHG']
             gc += row['FTAG']
         else:
-            if row['FTR'] == 'A':
-                pts += 3
-            elif row['FTR'] == 'D':
-                pts += 1
+            if row['FTR'] == 'A': pts += 3
+            elif row['FTR'] == 'D': pts += 1
             gs += row['FTAG']
             gc += row['FTHG']
-
+            
     return pts, gs / len(past_games), gc / len(past_games)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    prediction = None
+    home_prob = None
+    draw_prob = None
+    away_prob = None
     home_team = None
     away_team = None
 
@@ -52,14 +49,16 @@ def index():
         if home_team and away_team and home_team != away_team:
             h_pts, h_gs, h_gc = get_latest_form(home_team, df)
             a_pts, a_gs, a_gc = get_latest_form(away_team, df)
-
+            
             features = [[h_pts, h_gs, h_gc, a_pts, a_gs, a_gc]]
+            
+            probs = model.predict_proba(features)[0]
+            
+            away_prob = round(probs[0] * 100, 1)
+            draw_prob = round(probs[1] * 100, 1)
+            home_prob = round(probs[2] * 100, 1)
 
-            prob = model.predict_proba(features)[0][1]
-            prediction = round(prob * 100, 1)
-
-    return render_template('index.html', teams=teams, prediction=prediction, home_team=home_team, away_team=away_team)
-
+    return render_template('index.html', teams=teams, home_prob=home_prob, draw_prob=draw_prob, away_prob=away_prob, home_team=home_team, away_team=away_team)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
