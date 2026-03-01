a2c363a290a412ca8f73b50017655538b6032265
Carrick Kennedy <dkkmschxcw@privaterelay.appleid.com>
2026-03-01 15:23:18 +0000

    Update data_builder.py

--- Tacti/data_builder.py
+++ Tacti/data_builder.py
@@ -11,53 +11,49 @@ for season in seasons:
         temp_df['Date'] = pd.to_datetime(temp_df['Date'], dayfirst=True)
         dfs.append(temp_df)
     except Exception as e:
-        print(f"Skipping {season}")
+        pass
 
 df = pd.concat(dfs, ignore_index=True)
 df = df.sort_values(by='Date')
 df = df.dropna(subset=['FTR'])
 
-
-def get_form(team, date, data):
-    past_games = data[((data['HomeTeam'] == team) | (data['AwayTeam'] == team)) & (data['Date'] < date)].tail(3)
-
-    if len(past_games) == 0:
+def get_ewma_form(team, date, data):
+    past_games = data[((data['HomeTeam'] == team) | (data['AwayTeam'] == team)) & (data['Date'] < date)].tail(5)
+    
+    if len(past_games) < 5:
         return 0, 0, 0
-
+    
     pts = 0
     gs = 0
     gc = 0
-
-    for _, row in past_games.iterrows():
+    weights = [0.10, 0.15, 0.20, 0.25, 0.30]
+    
+    for i, (_, row) in enumerate(past_games.iterrows()):
+        w = weights[i]
         if row['HomeTeam'] == team:
-            if row['FTR'] == 'H':
-                pts += 3
-            elif row['FTR'] == 'D':
-                pts += 1
-            gs += row['FTHG']
-            gc += row['FTAG']
+            if row['FTR'] == 'H': pts += 3 * w
+            elif row['FTR'] == 'D': pts += 1 * w
+            gs += row['FTHG'] * w
+            gc += row['FTAG'] * w
         else:
-            if row['FTR'] == 'A':
-                pts += 3
-            elif row['FTR'] == 'D':
-                pts += 1
-            gs += row['FTAG']
-            gc += row['FTHG']
-
-    return pts, gs / len(past_games), gc / len(past_games)
-
+            if row['FTR'] == 'A': pts += 3 * w
+            elif row['FTR'] == 'D': pts += 1 * w
+            gs += row['FTAG'] * w
+            gc += row['FTHG'] * w
+            
+    return pts, gs, gc
 
 h_pts, h_gs, h_gc = [], [], []
 a_pts, a_gs, a_gc = [], [], []
 
 for index, row in df.iterrows():
-    hp, hg, hgc = get_form(row['HomeTeam'], row['Date'], df)
-    ap, ag, agc = get_form(row['AwayTeam'], row['Date'], df)
-
+    hp, hg, hgc = get_ewma_form(row['HomeTeam'], row['Date'], df)
+    ap, ag, agc = get_ewma_form(row['AwayTeam'], row['Date'], df)
+    
     h_pts.append(hp)
     h_gs.append(hg)
     h_gc.append(hgc)
-
+    
     a_pts.append(ap)
     a_gs.append(ag)
     a_gc.append(agc)
@@ -69,5 +65,7 @@ df['H_FormGC'] = h_gc
 df['A_FormPts'] = a_pts
 df['A_FormGS'] = a_gs
 df['A_FormGC'] = a_gc
+
+df = df[(df['H_FormPts'] > 0) & (df['A_FormPts'] > 0)]
 
 df.to_csv('match_features.csv', index=False)
